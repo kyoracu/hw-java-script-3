51f2cd190e7bf6624120a03ae0dcc3bdf4abee2a
kyoracu <116087865+kyoracu@users.noreply.github.com>
2022-11-05 15:50:02 +0600

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,28 @@
+// function stepen(a,b){
+//     return a**b
+// }
+// let a = 4;
+// let b = 2;
+
+// console.log(stepen(a,b))
+
+// let mass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// let mass2 = [];
+
+// for (let i = 0; i <= mass.length; i++) {
+//     let f = mass[i] % 2;
+//     if (f == 0){
+//        mass2.push(mass[i])
+//     }
+// }
+// console.log(mass2)
+
+// !!!Чтобы найти среднее арифметическое, нужно сложить все числа и поделить их сумму на их количество.
+
+function credne(a,b){
+    return (a+b)/2
+}
+let a = 4;
+let b = 2;
+
+console.log(credne(a,b))
